34435810c286d3099c402dd554f988e50e416ea9
SamantaSmith <teerexchannel@gmail.com@>
2019-06-28 08:10:56 +0300

    v 3.2

--- app/src/main/java/com/example/notes1/NewNoteActivity.java
+++ app/src/main/java/com/example/notes1/NewNoteActivity.java
@@ -1,14 +1,63 @@
 package com.example.notes1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class NewNoteActivity extends AppCompatActivity {
 
+    private Button btnCreate;
+    private EditText etTitle, etContent;
+    private Toolbar mToolbar;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+        btnCreate = (Button) findViewById(R.id.new_note_btn);
+        etTitle = (EditText) findViewById(R.id.new_note_title);
+        etContent = (EditText) findViewById(R.id.new_note_content);
+        mToolbar = (Toolbar) findViewById(R.id.new_note_toolbar);
+
+        setSupportActionBar(mToolbar);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String title = etTitle.getText().toString().trim();
+                String content = etContent.getText().toString().trim();
+
+                if (TextUtils.isEmpty(title) && !TextUtils.isEmpty(content)) {
+
+                    createNote();
+                } else {
+                    Snackbar.make(view, "Fill empty fields", Snackbar.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+
+
+    }
+
+    private void createNote(){
+
+
     }
 }
